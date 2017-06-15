f78957aee50347f0355c662d5deb702b6b2899ab
Talima <teamtalima@gmail.com>
2017-06-15 01:20:25 -0600

    In Progress, Minigame #1 - Block Stack
    Problems with position/localPosition synchronization between clients.
    Owner of minigameBlock moves and views movement of owned block
    correctly, all other clients experience rubberbanding.

---
+++ Assets/_theGAME/scripts/Minigames/minigameBlock.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using Photon;
+
+public class minigameBlock : PunBehaviour {
+
+    private Transform console;
+    private Rigidbody rb;
+
+    public PlayerController owner;
+    public bool hasOwner = false;
+
+    // Use this for initialization
+    void Start () {
+        console = transform.parent;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.isWriting) {
+            stream.SendNext(hasOwner);
+        } else {
+            hasOwner = (bool)stream.ReceiveNext();
+        }
+    }
+
+    public void Interact(PlayerController player) {
+        if(hasOwner == false) {
+            if (player.heldItem != null) return;
+
+            if (PhotonNetwork.player != photonView.owner) {
+                Debug.Log("Request viewOwnership of Block");
+                photonView.RequestOwnership();
+            }
+
+            Debug.Log("Block is being picked up by player");
+            photonView.RPC("PickUpBlock", PhotonTargets.All, player.photonView.viewID);
+        } else if(hasOwner == true && player == owner) {
+            
+            Debug.Log("Block is being DROPPED by player: " + player.name);
+            photonView.RPC("DropBlock", PhotonTargets.All);
+        } else {
+            Debug.Log("Attempt made to steal block");
+        }
+    }
+
+    [PunRPC]
+    void PickUpBlock(int playerViewID) {
+        hasOwner = true;
+        owner = PhotonView.Find(playerViewID).GetComponent<PlayerController>();
+        owner.heldItem = gameObject;
+        
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        Transform t = owner.gameObject.transform;
+        transform.SetParent(t);
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = new Vector3(0.0f, 0.5f, 1.1f);
+    }
+
+    [PunRPC]
+    void DropBlock() {
+        hasOwner = false;
+        owner.heldItem = null;
+        owner = null;
+
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        transform.SetParent(console.transform);
+    }
+}
---
+++ Assets/_theGAME/scripts/Minigames/minigameBlockStack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class minigameBlockStack : PunBehaviour {
+
+    // Block Prefab
+    public GameObject block;
+
+    // Is minigame being played?
+    private bool gameActive;
+
+    public int blockCount = 5;
+    public float blockSpawnHeight = 2;
+
+    // Use this for initialization
+    void Start () {
+        if (block == null) Debug.LogError("Minigame Console's 'block' prefab = null");
+
+        gameActive = false;
+    }
+
+    void Update() {
+        if (gameActive != true) return;
+
+        if (Physics.OverlapBox(transform.position + Vector3.up * 2f, new Vector3(.1f, 3f, .1f), Quaternion.identity, 1 << LayerMask.NameToLayer("Interact")).Length >= 3) {
+            Debug.Log("WE WIN!!");
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.isWriting) {
+            stream.SendNext(gameActive);
+        } else {
+            gameActive = (bool)stream.ReceiveNext();
+        }
+    }
+
+    public void Interact() {
+        if (gameActive == false) {
+            gameActive = true;
+            Debug.Log("Starting minigame!");
+
+            Vector3 offset = new Vector3(0, blockSpawnHeight, 0);
+            for (int i = 0; i < blockCount; i++) {
+                offset.x = Random.Range(-5, 5);
+                offset.z = Random.Range(-5, 5);
+                GameObject block = PhotonNetwork.Instantiate("Minigame Block", transform.position + offset, Quaternion.identity, 0);
+                photonView.RPC("SetBlockParent", PhotonTargets.AllBuffered, block.GetPhotonView().viewID);
+            }
+        } else {
+            Debug.Log("Minigame in progress");
+        }
+    }
+
+    [PunRPC]
+    void SetBlockParent(int blockViewID) {
+        PhotonView.Find(blockViewID).transform.SetParent(transform);
+    }
+
+    public void MinigameWin() {
+
+    }
+}

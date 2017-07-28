e81b3d384ad0db9ab48dcdfa7ce01f9086ea725c
Talima <teamtalima@gmail.com>
2017-07-27 20:07:18 -0600

    Added GUIManager script

---
+++ Assets/_theGAME/scripts/Managers/GUIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
+
+// Move CameraController GUI checks here
+// Combine Player Prefabs' username canvases into one (controlled here)
+// Move/Check Disconnect btn
+// Move/Check Bawesomeness text
+// Make singleton
